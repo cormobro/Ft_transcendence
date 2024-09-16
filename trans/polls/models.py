4b563bb5d282624bfefced08ab60bceb42c62587
mdor <maxdor@LAPTOP-R0KAPKCI>
2024-09-16 15:33:05 +0200

    t

--- trans/polls/models.py
+++ trans/polls/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password
 import datetime
 import uuid
 
@@ -8,8 +11,14 @@ import uuid
 
 class Player(models.Model):
     username = models.CharField(max_length=100, unique=True, blank=True)
+	password = models.CharField(max_length=120)
     scores = ArrayField(models.IntegerField(), default=list, blank=True)
     tournaments = models.ManyToManyField('Tournament', related_name='players', blank=True)
+
+	def set_password(self, raw_password):
+		self.password = make_password(raw_password)
+		self.save()
+		
     def __str__(self):
         return self.username
 
