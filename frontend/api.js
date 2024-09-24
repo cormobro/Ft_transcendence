644ee467e1b7b440793bdc79d913015e25c28894
Félix Bonaert <fbonaert@Felixs-MacBook-Air.local>
2024-09-24 18:35:41 +0200

    API calls

--- frontend/api.js
+++ frontend/api.js
@@ -26,3 +26,205 @@ document.getElementById('postRequest').onclick = () => {
 	formdata.append('age', '26')
 	requestObj.send(formdata)
 }
+
+document.getElementById('/post/match').onclick = () => {
+	/*const params =
+	{
+		player1: player1,
+		player2: player2,
+		gamemode: gameMode,
+		winner: winner,
+		player1score: leftScore,
+		player2score: rightScore,
+		date: matchDebut,
+		duration: Date.now() - matchDebut
+	};
+	const options =
+	{
+		method: 'POST',
+		body: JSON.stringify( params )
+	};*/
+	fetch("/post/match", {
+		method: "POST",
+		body: JSON.stringify({
+			player1: player1,
+			player2: player2,
+			gamemode: gameMode,
+			winner: winner,
+			player1score: leftScore,
+			player2score: rightScore,
+			date: matchDebut,
+			duration: Date.now() - matchDebut
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/post/login').onclick = () => {
+	fetch("/post/login", {
+		method: "POST",
+		body: JSON.stringify({
+			username: /*USERNAME HERE*/,
+			password: /*PASSWORD HERE*/
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/post/changeusername').onclick = () => {
+	fetch("/post/changeusername", {
+		method: "POST",
+		body: JSON.stringify({
+			username: /*NEW USERNAME HERE*/
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/post/changepassword').onclick = () => {
+	fetch("/post/changepassword", {
+		method: "POST",
+		body: JSON.stringify({
+			password: /*NEW PASSWORD HERE*/
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+/* AVATAR UPLOADING
+document.getElementById('/post/changeavatar').onclick = () => {
+	fetch("/post/changeavatar", {
+		method: "POST",
+		body: JSON.stringify({
+			username: //USERNAME HERE
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}*/
+
+document.getElementById('/post/42api').onclick = () => {
+	fetch("/post/42api", {
+		method: "POST",
+		body: JSON.stringify({
+			42name: /*42NAME HERE*/
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/post/friend/add').onclick = () => {
+	fetch("/post/friend", {
+		method: "POST",
+		body: JSON.stringify({
+			type: "request",
+			username: /*USERNAME HERE*/
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/post/friend/accept').onclick = () => {
+	fetch("/post/friend", {
+		method: "POST",
+		body: JSON.stringify({
+			type: "accept",
+			username: /*USERNAME HERE*/
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/post/friend/decline').onclick = () => {
+	fetch("/post/friend", {
+		method: "POST",
+		body: JSON.stringify({
+			type: "decline",
+			username: /*USERNAME HERE*/
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/get/personalstats').onclick = () => {
+	fetch("/get/personalstats", {
+		method: "GET",
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/get/profile').onclick = () => {
+	fetch("/get/profile", {
+		method: "GET",
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
+
+document.getElementById('/get/user').onclick = () => {
+	fetch("/get/user", {
+		method: "GET",
+		body: JSON.stringify({
+			username: //USERNAME HERE
+		}),
+		headers: {
+			"X-CSRF-Token": csrftoken,
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+}
