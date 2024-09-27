8663c2f8226d29eff62a58b0086411173f5cc2db
mdor <maxdor@LAPTOP-R0KAPKCI>
2024-09-27 16:47:23 +0200

    date temps

--- trans/polls/models.py
+++ trans/polls/models.py
@@ -46,3 +46,5 @@ class Match(models.Model):
 	result_player1 = models.CharField(max_length=255, blank=False)
 	player1_points = models.IntegerField()
 	player2_points = models.IntegerField()
+	date = models.DateField()
+	match_time = models.DurationField()
